c7df5a58defd5be9049528d1b809b6daa27a0e46
webbandz <tuaman027@aol.com>
2021-03-08 08:49:02 -0500

    Thanksgiving!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Is it burning?! Well, see?!  Now you are learning!'));
+app.get('/', (req, res) => res.send('I got greens, beans, potatoes, tomatoes, lamb, yam...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
